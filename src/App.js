34b3ca63b84ac92de2904f24fc138d3a4c529ad2
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-13 23:47:15 -0500

    Add Game component to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Game from './components/Game';
 
 class App extends Component {
   state = {
     score: 0,
-    topScore: 0
+    topScore: 0,
+    images: [
+      'bass', 'clarinet', 'cymbals', 'flute', 'marimba', 'mellophone',
+      'quads', 'sax', 'snare', 'trombone', 'trumpet', 'tuba'
+    ]
+  }
+
+  componentDidMount() {
+    this.setState({images: this.shuffleArr(this.state.images)});
+  }
+
+  shuffleArr = arr => {
+    let ctr = arr.length,
+      temp,
+      i;
+    
+    // While there are elements in the array
+    while (ctr > 0) {
+      // Pick a random index
+      i = Math.floor(Math.random() * ctr);
+      // Decrease ctr by 1
+      ctr--;
+
+      // And swap the last element with it
+      temp = arr[ctr];
+      arr[ctr] = arr[i];
+      arr[i] = temp;
+    }
+
+    return arr;
   }
 
   // increment the score
@@ -28,6 +58,7 @@ class App extends Component {
       <div>
         <Navbar score={this.state.score} topScore={this.state.topScore}/>
         <Header />
+        <Game images={this.state.images} />
       </div>
     )
   }
